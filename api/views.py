9ac58ed3b5f4ac553fa5be6a6eeeee921b09f1dd
vannguyenn1999 <vannguyenn2809@gmail.com>
2025-05-19 19:01:22 +0700

    up

--- api/views.py
+++ api/views.py
@@ -16,6 +16,7 @@ class BaseModelViewSet(viewsets.ModelViewSet):
 
     def get_permissions(self):
         if self.action in ['update', 'destroy']:
+            return [AllowAny()]
             return [IsAuthenticated()]
         return [AllowAny()]
 
